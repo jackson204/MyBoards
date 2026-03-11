bd93796ce8a28d3f2bb9e8f5784b610518f63a55
jackson <s97403002a@gmail.com>
2026-03-11 23:04:32 +0800

    feat(ef-core): 設定 DbContext 使用 SQL Server LocalDB
    覆寫 OnConfiguring 方法，明確指定連線至 MyBoardsDb 資料庫。

--- MyBoards/Entities/MyBoardsContext.cs
+++ MyBoards/Entities/MyBoardsContext.cs
@@ -4,6 +4,12 @@ namespace MyBoards.Entities;
 
 public class MyBoardsContext : DbContext
 {
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MyBoardsDb;Trusted_Connection=True;");
+        base.OnConfiguring(optionsBuilder);
+    }
+
     public DbSet<WorkItem> WorkItems { get; set; }
 
     public DbSet<User> Users { get; set; }
