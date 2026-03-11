74fec7ab1188e812aadb515f22c6b8ab530e61bb
jackson <s97403002a@gmail.com>
2026-03-11 21:02:36 +0800

    feat(WorkItem): 擴充工作項目實體屬性
    為 WorkItem 實體新增 State, Area, IterationPath, Priority 等通用屬性，並加入 StartDate, EndDate (Epic), Efford (Issue), Activity, RemainingWork (Task) 等特定類型屬性，以支援更全面的工作項目資料模型。

--- MyBoards/Entities/WorkItem.cs
+++ MyBoards/Entities/WorkItem.cs
@@ -2,5 +2,24 @@ namespace MyBoards.Entities;
 
 public class WorkItem
 {
-    
+    public string State { get; set; }
+
+    public string Area { get; set; }
+
+    public string IterationPath { get; set; }
+
+    public string Priority { get; set; }
+
+    //Epic
+    public DateTime? StartDate { get; set; }
+
+    public DateTime? EndDate { get; set; }
+
+    //Issue
+    public decimal Efford { get; set; }
+
+    //Task
+    public string Activity { get; set; }
+
+    public decimal RemainingWork { get; set; }
 }
