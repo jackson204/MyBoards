8981d7c15f7c4a4fc5bfd713387897b4e3903a74
jackson <s97403002a@gmail.com>
2026-03-11 21:11:49 +0800

    feat(efcore): 配置 MyBoardsContext 並定義實體集
    使 MyBoardsContext 繼承 DbContext，啟用 Entity Framework Core 功能。
    新增 WorkItem、User、Tag、Comment 及 Address 的 DbSet 屬性。
    引入 Microsoft.EntityFrameworkCore.SqlServer 套件以支援 SQL Server。

--- MyBoards/Entities/MyBoardsContext.cs
+++ MyBoards/Entities/MyBoardsContext.cs
@@ -1,6 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace MyBoards.Entities;
 
-public class MyBoardsContext
+public class MyBoardsContext : DbContext
 {
-    
+    public DbSet<WorkItem> WorkItems { get; set; }
+
+    public DbSet<User> Users { get; set; }
+
+    public DbSet<Tag> Tags { get; set; }
+
+    public DbSet<Comment> Comments { get; set; }
+
+    public DbSet<Address> Addresses { get; set; }
 }
